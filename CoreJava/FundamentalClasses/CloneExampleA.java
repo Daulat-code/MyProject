aaa0b97860d3f77be20a1b6ef6ea67868ee27954
Daulatram kushwah <daulat.java51@gmail.com>
2025-03-29 19:20:49 +0530

    Clone method example

---
+++ CoreJava/FundamentalClasses/CloneExampleA.java
@@ -0,0 +1,31 @@
+package FundamentalClasses;
+
+public class CloneExampleA implements Cloneable {
+	int x=10,y=20;
+	public static void main(String[] args) throws CloneNotSupportedException
+	{
+		// TODO Auto-generated method stub
+		
+		CloneExampleA e1=new CloneExampleA();
+		
+		e1.x=5;
+		e1.y=6;
+		
+		CloneExampleA e2= (CloneExampleA)e1.clone();
+		System.out.println(e1==e2);
+		System.out.println(e1.hashCode()==e2.hashCode());
+		
+		System.out.println(e1.x+"..."+e1.y);
+		System.out.println(e2.x+"..."+e2.y);
+		
+		e2.x=8;
+		e2.y=9;
+		
+		System.out.println(e1.x+"..."+e1.y);
+		System.out.println(e2.x+"..."+e2.y);
+		
+		
+
+	}
+
+}
